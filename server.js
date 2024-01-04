05b590fd0a10bddeb097f9f437191cbaeb1e6f4c
Kai <akaikainazar@gmail.com>
2024-01-04 22:52:40 +0600

    initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const userRoutes = require('./routes/user')
+const cors = require('cors')
+
+// express app
+const app = express()
+
+// connect to db
+mongoose
+    .connect(process.env.MONG_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db & listening to port', process.env.PORT)
+        })
+    })
+    .catch((err) => console.log(err))
+
+// middleware
+app.use(express.json())
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+app.use(
+    cors({
+        origin: 'https://tamaqdana.onrender.com',
+    })
+)
+
+// routes
+app.use('', userRoutes)
